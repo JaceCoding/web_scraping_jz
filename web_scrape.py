2fc2465e97042b73838b7f054b186581e23cda46
jace <jace.zhg@gmail.com>
2024-06-24 03:15:27 +1000

    created global variable for user friendliness

--- web_scrape.py
+++ web_scrape.py
@@ -2,14 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 from fpdf import FPDF
 
-# Initialize PDF class instance
-pdf = FPDF()
+# Input novel name with underline e.g. swallowed_star
+novel_name = 'swallowed_star'
 
-# Add an initial page
+# Latest chapter of the novel
+max_chapter = 1486
+
+
+pdf = FPDF()
 pdf.add_page()
 
-for i in range(1, 1486):
-    url = f'https://fast.novelcenter.net/novel-book/swallowed-star/chapter-{i}'
+for i in range(1, max_chapter):
+    url = f'https://fast.novelcenter.net/novel-book/{novel_name}/chapter-{i}'
 
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -24,16 +28,13 @@ for i in range(1, 1486):
     pdf.add_font('DejaVu', '', 'DejaVuSans.ttf')
     pdf.set_font('DejaVu', '', 12)
 
-    # Add text content to the PDF with 10 lines (50 units) of space between paragraphs
     for paragraph in text_content:
         pdf.multi_cell(0, 5, paragraph)
-        pdf.ln(5)  # Add a new line with a height of 10 units
+        pdf.ln(5)
 
-    # Add 10 lines of space between chapters
     pdf.ln(50)
 
-# Save the PDF to a file
-pdf_output_path = 'swallowed_star.pdf'
+pdf_output_path = f'{novel_name}.pdf'
 pdf.output(pdf_output_path)
 
 print(f"PDF created successfully and saved as {pdf_output_path}")
